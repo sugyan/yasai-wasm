226b7caa73237d6f6a36ebccd97b8f97c25e3357
sugyan <sugi1982@gmail.com>
2022-07-03 23:01:23 +0900

    Update webpack.config.js

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,5 +1,5 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: "./bootstrap.js",
@@ -9,6 +9,15 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html', 'perft.html'])
+    new CopyWebpackPlugin([
+      "index.html",
+      "perft.html",
+      "worker.js",
+      "simd_worker.js",
+      { from: "pkg/yasai_wasm.js", to: "pkg" },
+      { from: "pkg/yasai_wasm_bg.wasm", to: "pkg" },
+      { from: "pkg-simd/yasai_wasm.js", to: "pkg-simd" },
+      { from: "pkg-simd/yasai_wasm_bg.wasm", to: "pkg-simd" },
+    ]),
   ],
 };
